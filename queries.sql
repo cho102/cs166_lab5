dfd3e783ff66b47b1b8648135cdeca14d216cf7e
Cindy Ho <cho102@wch135-04.cs.ucr.edu>
2022-10-21 16:49:55 -0700

    finished lab

--- queries.sql
+++ queries.sql
@@ -8,11 +8,12 @@ WHERE C.cost < 10 AND P.pid = C.pid;
 
 SELECT S.address
 FROM PARTS P, CATALOG C, SUPPLIERS S
-WHERE P.pname = "Fire Hydrant Cap";
+WHERE P.pname = 'Fire Hydrant Cap' AND P.pid = C.pid AND S.sid = C.sid;
 
 SELECT S.sname
 FROM PARTS P, CATALOG C , SUPPLIERS S
-WHERE P.color = "green";
+WHERE P.color = 'Green' AND P.pid = C.pid AND S.sid = C.sid;
 
 SELECT S.sname, P.pname
-FROM PARTS P, CATALOG C, SUPPLIERS S;
+FROM PARTS P, CATALOG C, SUPPLIERS S
+WHERE P.pid = C.pid aND S.sid = C.sid;
